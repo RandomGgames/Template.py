1c250901a81cb15407ed778143d0fbf300f13aa7
RandomGgames <gianopp@gmail.com>
2025-06-10 15:27:01 -0400

    Update SN Template.py

--- SN Template.py
+++ SN Template.py
@@ -17,14 +17,14 @@ Python Script Template
 
 def main(dut_sn: str | None) -> None:
     start_time = time.perf_counter()
-    logger.info('Starting operation...')
-    logger.debug(f'Testing DUT SN "{dut_sn}"')
+    logger.info("Starting operation...")
+    logger.debug(f"Testing DUT SN '{dut_sn}'")
 
     pass
 
     end_time = time.perf_counter()
     duration = end_time - start_time
-    logger.info(f'Completed operation in {duration:.4f}s.')
+    logger.info(f"Completed operation in {duration:.4f}s.")
 
 
 def setup_logging(
@@ -33,8 +33,8 @@ def setup_logging(
         number_of_logs_to_keep: typing.Union[int, None] = None,
         console_logging_level: int = logging.DEBUG,
         file_logging_level: int = logging.DEBUG,
-        log_message_format: str = '%(asctime)s.%(msecs)03d %(levelname)s [%(funcName)s] [%(name)s]: %(message)s',
-        date_format: str = '%Y-%m-%d %H:%M:%S') -> None:
+        log_message_format: str = "%(asctime)s.%(msecs)03d %(levelname)s [%(funcName)s] [%(name)s]: %(message)s",
+        date_format: str = "%Y-%m-%d %H:%M:%S") -> None:
     # Ensure log_dir is a Path object
     log_file_path = pathlib.Path(log_file_path)
     log_dir = log_file_path.parent
@@ -42,7 +42,7 @@ def setup_logging(
 
     # Limit # of logs in logs folder
     if number_of_logs_to_keep is not None:
-        log_files = sorted([f for f in log_dir.glob('*.log')], key=lambda f: f.stat().st_mtime)
+        log_files = sorted([f for f in log_dir.glob("*.log")], key=lambda f: f.stat().st_mtime)
         if len(log_files) >= number_of_logs_to_keep:
             for file in log_files[:len(log_files) - number_of_logs_to_keep + 1]:
                 file.unlink()
@@ -50,7 +50,7 @@ def setup_logging(
     logger.setLevel(file_logging_level)  # Set the overall logging level
 
     # File Handler for date-based log file
-    file_handler_date = logging.FileHandler(log_file_path, encoding='utf-8')
+    file_handler_date = logging.FileHandler(log_file_path, encoding="utf-8")
     file_handler_date.setLevel(file_logging_level)
     file_handler_date.setFormatter(logging.Formatter(log_message_format, datefmt=date_format))
     logger.addHandler(file_handler_date)
@@ -62,15 +62,15 @@ def setup_logging(
     logger.addHandler(console_handler)
 
     # Set specific logging levels if needed
-    # logging.getLogger('requests').setLevel(logging.INFO)
+    # logging.getLogger("requests").setLevel(logging.INFO)
 
 
 if __name__ == "__main__":
-    dut_sn = input(f'SN: ')
-    if dut_sn == '':
+    dut_sn = input(f"SN: ")
+    if dut_sn == "":
         dut_sn = None
     pc_name = socket.gethostname()
-    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     script_name = pathlib.Path(__file__).stem
     log_dir = pathlib.Path(f"{script_name} Logs/{dut_sn}")
     log_file_name = f"{timestamp}_{str(dut_sn)}_{pc_name}.log"
@@ -81,7 +81,7 @@ if __name__ == "__main__":
     try:
         main(dut_sn)
     except Exception as e:
-        logger.warning(f'A fatal error has occurred: {repr(e)}\n{traceback.format_exc()}')
+        logger.warning(f"A fatal error has occurred: {repr(e)}\n{traceback.format_exc()}")
         error = 1
     finally:
         sys.exit(error)
